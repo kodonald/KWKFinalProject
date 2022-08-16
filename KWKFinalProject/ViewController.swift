51c28a572abd963d456d127d0e940e9adaf658f9
Kibali O'Donald <kibali.odonald@gmail.com>
2022-08-16 14:18:15 -0400

    Initial Commit

---
+++ KWKFinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KWKFinalProject
+//
+//  Created by Kibali O’Donald on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
